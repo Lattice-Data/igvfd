b0758d84d50ee35dd929e295a60038ba764a7e01
Keenan Graham <ksgraham@stanford.edu>
2025-10-23 14:15:45 -0700

    Point to snapshot arn

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -201,7 +201,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': PROD_DATABASE_IDENTIFIER,
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:159466469043:snapshot:bootstrap-10-23-25',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
@@ -270,7 +270,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': PROD_DATABASE_IDENTIFIER,
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:159466469043:snapshot:bootstrap-10-23-25',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
@@ -339,6 +339,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:159466469043:snapshot:bootstrap-10-23-25',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
--- cdk/infrastructure/constants.py
+++ cdk/infrastructure/constants.py
@@ -1,3 +1,3 @@
-DEV_DATABASE_IDENTIFIER = 'igvfd-dev-deploydevelopme-postgrespostgresfromsnap-kvennmzadzpr'
+DEV_DATABASE_IDENTIFIER = 'igvfd-dev-deploydevelopment-postg-postgres97b73533-sqfv5tf4kos8'
 
 PROD_DATABASE_IDENTIFIER = 'igvfd-main-productiondeploystage-postgres97b73533-ikxchjbjh9yk'
