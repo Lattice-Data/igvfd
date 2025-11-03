c6736f255f6c1a992aeb593bc0c37334cc73b4c2
Idan Gabdank <gabdank@stanford.edu>
2025-11-03 10:59:30 -0800

    Remove 'schema_version' field from image fixture to streamline test data structure and enhance clarity.

--- src/igvfd/tests/fixtures/schemas/image.py
+++ src/igvfd/tests/fixtures/schemas/image.py
@@ -7,7 +7,6 @@ def image(testapp):
     item = {
         'attachment': {'download': 'red-dot.png', 'href': RED_DOT},
         'caption': 'A red dot image for testing.',
-        'schema_version': '1',
         'status': 'current'
     }
     return testapp.post_json('/image', item, status=201).json['@graph'][0]
