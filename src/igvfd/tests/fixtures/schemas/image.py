42214a2c1d1659ffa441795f4cdc62b05056adac
Idan Gabdank <gabdank@stanford.edu>
2025-11-03 11:06:40 -0800

    Remove caption field from image fixture to simplify test data structure and enhance clarity.

--- src/igvfd/tests/fixtures/schemas/image.py
+++ src/igvfd/tests/fixtures/schemas/image.py
@@ -6,7 +6,6 @@ from ...constants import *
 def image(testapp):
     item = {
         'attachment': {'download': 'red-dot.png', 'href': RED_DOT},
-        'caption': 'A red dot image for testing.',
         'status': 'current'
     }
     return testapp.post_json('/image', item, status=201).json['@graph'][0]
