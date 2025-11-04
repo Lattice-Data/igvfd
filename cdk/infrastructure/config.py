1134937f07ac4ac2fc16b9192146720e30126e58
ottojolanki <ojolanki@stanford.edu>
2025-11-04 12:20:57 -0800

    staging-dev-ttl (#11)

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -43,6 +43,8 @@ config: Dict[str, Any] = {
             'existing_resources_class': igvf_dev.Resources,
             'account_and_region': igvf_dev.US_WEST_2,
             'tags': [
+                ('time-to-live-hours', '60'),
+                ('turn-off-on-friday-night', 'yes'),
             ],
         },
         'production': {
@@ -192,6 +194,8 @@ config: Dict[str, Any] = {
                 'arn': 'arn:aws:wafv2:us-west-2:159466469043:regional/webacl/Lattice2ApiDemoWaf-Wi9CUpMXmj0O/9db3bb63-c6b5-4963-961e-2d4f628f8ce0',
             },
             'tags': [
+                ('time-to-live-hours', '60'),
+                ('turn-off-on-friday-night', 'yes'),
             ]
         },
         'staging': {
@@ -260,6 +264,8 @@ config: Dict[str, Any] = {
                 'arn': 'arn:aws:wafv2:us-west-2:555476105356:regional/webacl/Lattice2ApiStagingWaf-CCeWTQGgZ4mP/d831aaac-1160-4acb-b6f9-2a17737a2627',
             },
             'tags': [
+                ('time-to-live-hours', '60'),
+                ('turn-off-on-friday-night', 'yes'),
             ],
             'url_prefix': 'api',
         },
