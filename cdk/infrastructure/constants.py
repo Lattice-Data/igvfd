fe3d20b72fdad9ca1cddf5f4c958f7c9be7b62ab
Otto Jolanki <ojolanki@stanford.edu>
2026-01-07 12:41:40 -0800

    update constants and tests, remove snapshot from prod

--- cdk/infrastructure/constants.py
+++ cdk/infrastructure/constants.py
@@ -1,3 +1,3 @@
 DEV_DATABASE_IDENTIFIER = ''
 
-PROD_DATABASE_IDENTIFIER = 'igvfd-main-productiondepl-postgrespostgresfromsnap-gksqi3sgyvzf'
+PROD_DATABASE_IDENTIFIER = 'igvfd-main-productiondepl-postgrespostgresfromsnap-ct3gixgrlmzo'
