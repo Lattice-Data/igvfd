be7b5485b9cb1984f3d99fbc7c83824bf378bf66
Keenan Graham <ksgraham@stanford.edu>
2026-01-30 20:22:25 -0800

    Update

--- cdk/infrastructure/constants.py
+++ cdk/infrastructure/constants.py
@@ -1,3 +1,3 @@
 DEV_DATABASE_IDENTIFIER = ''
 
-PROD_DATABASE_IDENTIFIER = 'igvfd-main-productiondepl-postgrespostgresfromsnap-ct3gixgrlmzo'
+PROD_DATABASE_IDENTIFIER = 'igvfd-main-productiondepl-postgrespostgresfromsnap-2jhzalb6ftuh'
