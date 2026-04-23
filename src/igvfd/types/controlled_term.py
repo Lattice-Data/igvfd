82c9446ed589f7d8f80b35155411058baca139eb
Idan Gabdank <gabdank@users.noreply.github.com>
2026-04-23 12:29:25 -0700

    Ontology update (#68)
    * add user for local stack deploy
    
    * change controlled term data inserts to real terms
    
    * add further ontologies to controlled term schema json
    
    * update ontology.json with cog parsing
    
    * working calculated definition and synonyms for controlled terms
    
    * term name as calculated property
    
    * add anti-uniprot regex and data insert
    
    * add other ontologies for controlled terms
    
    * linting and pre-commit clean up
    
    * rerun mappings
    
    * Refactor controlled term tests and schema: update summary logic to return term_id, adjust test cases for term_name and aliases, and modify controlled_term fixture data for consistency.
    
    * Update controlled_term.json with new hash and index name for improved data integrity
    
    * Update controlled_term.json regex pattern to include 'uniprot' and 'anti-uniprot'; remove term_name assertions from tests for consistency.
    
    ---------
    
    Co-authored-by: Brian Mott <125435496+brian-mott@users.noreply.github.com>

--- src/igvfd/types/controlled_term.py
+++ src/igvfd/types/controlled_term.py
@@ -21,17 +21,77 @@ class ControlledTerm(Item):
     name_key = 'term_id'
     schema = load_schema('igvfd:schemas/controlled_term.json')
 
+    @staticmethod
+    def _get_ontology_string(registry, term_id, string_key):
+        if term_id not in registry['ontology']:
+            return ''
+        return registry['ontology'][term_id].get(string_key, '')
+
+    @staticmethod
+    def _get_ontology_slims(registry, term_id, slim_key):
+        if term_id not in registry['ontology']:
+            return []
+        key = registry['ontology'][term_id].get(slim_key, [])
+        return sorted(set(
+            slim for slim in key
+        )) or None
+
     @calculated_property(
+        condition='term_id',
         schema={
-            'title': 'Summary',
+            'title': 'Term Name',
             'type': 'string',
-            'description': 'A summary of the controlled term.',
+            'description': 'Human readable name for the ontology term',
             'notSubmittable': True,
         }
     )
-    def summary(self, term_name=None, aliases=None):
-        if term_name:
-            return term_name
-        if aliases:
-            return aliases[0]
-        return self.uuid
+    def term_name(self, registry, term_id):
+        # uniprot terms can be "anti-{term_id}" for antibodies
+        term_prefix = 'anti-' if term_id.startswith('anti-') else ''
+        term_to_lookup = term_id[len(term_prefix):]
+        name = self._get_ontology_string(registry, term_to_lookup, 'label')
+        return term_prefix + name
+
+    @calculated_property(
+        condition='term_id',
+        schema={
+            'title': 'Definition',
+            'type': 'string',
+            'description': 'Definition for the term that was recorded in an ontology.',
+            'notSubmittable': True,
+        }
+    )
+    def definition(self, registry, term_id):
+        # uniprot terms can be "anti-{term_id}" for antibodies
+        term_prefix = 'anti-' if term_id.startswith('anti-') else ''
+        term_to_lookup = term_id[len(term_prefix):]
+        description = self._get_ontology_string(registry, term_to_lookup, 'description')
+        return term_prefix + description if description else ''
+
+    @calculated_property(
+        condition='term_id',
+        schema={
+            'title': 'Synonyms',
+            'type': 'array',
+            'description': 'Alternative names or synonyms for this term.',
+            'uniqueItems': True,
+            'items': {
+                'type': 'string',
+            },
+            'notSubmittable': True,
+        }
+    )
+    def synonyms(self, registry, term_id):
+        return self._get_ontology_slims(registry, term_id, 'synonyms')
+
+    @calculated_property(
+        condition='term_id',
+        schema={
+            'title': 'Summary',
+            'type': 'string',
+            'description': 'The ontology term identifier (same as term_id).',
+            'notSubmittable': True,
+        }
+    )
+    def summary(self, term_id):
+        return term_id
